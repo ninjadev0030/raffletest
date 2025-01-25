1365d9f5ed39d0528911d6bc0757471f3951e663
Ayadi Moni <ayadimoni225@gmail.com>
2025-01-25 08:58:58 -0800

    update get winner route

--- backend/routes/raffleRoutes.js
+++ backend/routes/raffleRoutes.js
@@ -7,13 +7,40 @@ const router = express.Router();
 
 router.post('/', upload.single('image'), createRaffle);
 router.get('/', async (req, res) => {
-    try {
-      const raffles = await Raffle.find({ isActive: true });
-      res.status(200).json(raffles);
-    } catch (error) {
-      res.status(500).json({ message: 'Error fetching raffles', error });
+  try {
+    const raffles = await Raffle.find({ isActive: true });
+    res.status(200).json(raffles);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching raffles', error });
+  }
+});
+
+router.get("/raffles/:raffleId/winner", async (req, res) => {
+  const { raffleId } = req.params;
+
+  try {
+    // Find the raffle by its ID
+    const raffle = await Raffle.findById(raffleId);
+
+    if (!raffle) {
+      return res.status(404).json({ error: "Raffle not found" });
     }
-  });
+
+    if (!raffle.winnerId) {
+      return res.status(400).json({ error: "Winner not determined yet" });
+    }
+
+    // Respond with the winner details
+    res.status(200).json({
+      winnerId: raffle.winnerId,
+      prize: parseFloat(raffle.prizePool * 0.9).toFixed(2),
+    });
+  } catch (error) {
+    console.error("Error fetching winner details:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 
   
 module.exports = router;
