4786c41ccb2982741117794bb928c8f7f6eeac7f
Ayadi Moni <ayadimoni225@gmail.com>
2025-01-25 14:18:18 -0800

    update server

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const https = require('https');
 const raffleRoutes = require('./routes/raffleRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
 const path = require('path');
 const app = express();
 const startCronJob = require('./cron/raffleCron'); // Import the cron function
-const agent = new https.Agent({
-    rejectUnauthorized: false, // Bypass SSL validation
-});
+
 // Middleware
-app.use(cors({
-    origin: 'https://185.28.22.25:5000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
-app.use('/api/raffles', raffleRoutes, { httpsAgent: agent });
-app.use('/api', ticketRoutes, { httpsAgent: agent });
+app.use('/api/raffles', raffleRoutes);
+app.use('/api', ticketRoutes);
 
 // Start the cron job
 startCronJob();
