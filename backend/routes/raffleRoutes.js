ac84e1a61581544af89379141e5ff34cd771fdd4
Ayadi Moni <ayadimoni225@gmail.com>
2025-01-25 08:52:29 -0800

    get raffle isActive true

--- backend/routes/raffleRoutes.js
+++ backend/routes/raffleRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/', upload.single('image'), createRaffle);
 router.get('/', async (req, res) => {
     try {
-      const raffles = await Raffle.find();
+      const raffles = await Raffle.find({ isActive: true });
       res.status(200).json(raffles);
     } catch (error) {
       res.status(500).json({ message: 'Error fetching raffles', error });
