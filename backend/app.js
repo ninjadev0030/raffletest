059129e44f48523b8186a4318c56578180a552da
Ayadi Moni <ayadimoni225@gmail.com>
2025-01-25 14:14:35 -0800

    update avoid https

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,9 @@ const ticketRoutes = require('./routes/ticketRoutes');
 const path = require('path');
 const app = express();
 const startCronJob = require('./cron/raffleCron'); // Import the cron function
-
+const agent = new https.Agent({
+    rejectUnauthorized: false, // Bypass SSL validation
+});
 // Middleware
 app.use(cors({
     origin: 'https://185.28.22.25:5000',
@@ -15,8 +17,8 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
-app.use('/api/raffles', raffleRoutes);
-app.use('/api', ticketRoutes);
+app.use('/api/raffles', raffleRoutes, { httpsAgent: agent });
+app.use('/api', ticketRoutes, { httpsAgent: agent });
 
 // Start the cron job
 startCronJob();
