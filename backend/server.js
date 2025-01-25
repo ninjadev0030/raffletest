5593bf499c50efd6127ad0bbe7be81876f1432f6
Ayadi Moni <ayadimoni225@gmail.com>
2025-01-25 10:39:35 -0800

    update to https

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 const dotenv = require('dotenv');
+const fs = require('fs');
+const https = require('https');
 const connectDB = require('./config/db');
 const app = require('./app');
+const path = require('path');
 
 // Load environment variables
 dotenv.config();
 
 // Connect to MongoDB
 connectDB();
-
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'certificates', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'certificates', 'cert.pem'))
+};
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+https.createServer(options, app).listen(5000, () => {
+  console.log('HTTPS Server running on https://localhost:5000');
 });
